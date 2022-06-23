3f42a1e990e6e5a6d966fbfeac20bdf6c3bb160a
Michael Lewin <migwellian@gmail.com>
2022-06-23 10:51:02 +0100

    adjusted friction settings

--- Assets/Tests/FrictionTest.cs
+++ Assets/Tests/FrictionTest.cs
@@ -24,12 +24,12 @@ public class FrictionTest
         float startHeight = roughCube.transform.position.y;
 
         Assert.AreEqual(roughCube.transform.position.z, smoothCube.transform.position.z, 1e-4f, "Both cubes should start at the same z-position");
-        Assert.AreEqual(startHeight, roughCube.transform.position.y, 1e-4f, "Rough cube should be on the ground");
-        Assert.AreEqual(startHeight, smoothCube.transform.position.y, 1e-4f, "Smooth cube should be on the ground");
-        Assert.AreEqual(0, roughCube.transform.rotation.eulerAngles.x, 0.1f, "Rough cube should be facing forwards");
-        Assert.AreEqual(0, smoothCube.transform.rotation.eulerAngles.x, 0.1f, "smooth cube should be facing forwards");
+        Assert.AreEqual(startHeight, roughCube.transform.position.y, 1e-2f, "Rough cube should be on the ground");
+        Assert.AreEqual(startHeight, smoothCube.transform.position.y, 1e-2f, "Smooth cube should be on the ground");
+        Assert.AreEqual(1, roughCube.transform.forward.z, 0.1f, "Rough cube should be facing forwards");
+        Assert.AreEqual(1, smoothCube.transform.forward.z, 0.1f, "smooth cube should be facing forwards");
 
-        Vector3 force = Vector3.forward * 5;
+        Vector3 force = Vector3.forward * 10;
         for (int frame = 0; frame < 2 * 50; frame++)
         {
             roughCube.GetComponent<Rigidbody>().AddForce(force);
@@ -41,9 +41,9 @@ public class FrictionTest
 
         Assert.Greater(roughCube.transform.position.z - startPosition, 1, "Both cubes should move");
         Assert.Greater(smoothCube.transform.position.z - roughCube.transform.position.z, 1, "Smooth cube should move further than rough cube");
-        Assert.AreEqual(startHeight, roughCube.transform.position.y, 1e-4f, "Rough cube should stay on the ground");
-        Assert.AreEqual(startHeight, smoothCube.transform.position.y, 1e-4f, "Smooth cube should stay on the ground");
-        Assert.AreEqual(0, roughCube.transform.rotation.eulerAngles.x, 0.1f, "Rough cube should not roll");
-        Assert.AreEqual(0, smoothCube.transform.rotation.eulerAngles.x, 0.1f, "Smooth cube should not roll");
+        Assert.AreEqual(startHeight, roughCube.transform.position.y, 1e-2f, "Rough cube should stay on the ground");
+        Assert.AreEqual(startHeight, smoothCube.transform.position.y, 1e-2f, "Smooth cube should stay on the ground");
+        Assert.AreEqual(1, roughCube.transform.forward.z, 0.1f, "Rough cube should not roll");
+        Assert.AreEqual(1, smoothCube.transform.forward.z, 0.1f, "Smooth cube should not roll");
     }
 }
